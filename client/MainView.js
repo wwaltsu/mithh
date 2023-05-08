aaa271c2cb1ddff335378b8a9adbaa99e58638ce
Waltsu <waltsumarcus@gmail.com>
2023-05-08 18:19:44 +0300

    css changes

--- client/MainView.js
+++ client/MainView.js
@@ -5,7 +5,7 @@ export const MainView = {
   view: function () {
     const divList = []
     for (let i = 1; i <= 10000; i++) {
-      divList.push(m("div", { "class": "itembox" }, "div numero: " + i))
+      divList.push(m("div", { "class": "item" }, "div numero: " + i))
     }
     return m("div.container", divList)
   }
